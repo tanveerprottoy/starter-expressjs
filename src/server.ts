04c2b39356113faa6dbfaa78338ebb39002f2162
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-04-14 16:01:11 +0600

    add cluster code

--- src/server.ts
+++ src/server.ts
@@ -30,3 +30,29 @@ export async function createServer() {
     });
     return app;
 }
+
+/* export async function createServerCluster() {
+    const numCPUs = availableParallelism();
+
+    if(cluster.isPrimary) {
+        console.log(`Primary ${process.pid} is running`);
+
+        // Fork workers.
+        for(let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`worker ${worker.process.pid} died`);
+        });
+    } else {
+        // Workers can share any TCP connection
+        // In this case it is an HTTP server
+        http.createServer((req, res) => {
+            res.writeHead(200);
+            res.end('hello world\n');
+        }).listen(8000);
+
+        console.log(`Worker ${process.pid} started`);
+    }
+} */
